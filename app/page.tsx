a2e462a3c35af14d477b289e06e879fbdeaca671
Atharva Sawant <athusawant7@gmail.com>
2026-01-26 00:32:20 +0530

    First part

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to my Saas</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Page
